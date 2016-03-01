05b0e4a84c0e08717f3f4a1795e5b1bfda73cbf4
avot ron <ronavot@h-mac-mini-sl-ron.local>
2016-03-01 18:01:18 +0200

    Initial Commit

---
+++ pokedex-by-ronAvot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-ronAvot
+//
+//  Created by ron avot on 1.3.2016.
+//  Copyright © 2016 ron avot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
